70351803f2484a5421ff2a2c394a5ab15933e38b
edKotinsky <edkotinsky@gmail.com>
2023-04-02 18:08:41 +0600

    First simple test for switch_argument

--- test/smoke_test.cpp
+++ test/smoke_test.cpp
@@ -1,8 +1,21 @@
+#include "argp/command_line_fwd.hpp"
 #include <argp/command_line.hpp>
 #include <cassert>
 
 int main() {
   int i = argp::utility::from_string<int>("123");
   assert(i == 123);
+
+  argp::command_line cmd("--", "-");
+  argp::switch_argument sw("hello", "h", cmd);
+
+  std::vector<std::string_view> vec = { "--hello" };
+
+  assert(sw.get() == false);
+
+  cmd.parse(vec);
+
+  assert(sw.get() == true);
+
   return 0;
 }

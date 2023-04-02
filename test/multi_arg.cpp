7b16350bccb0447075fb41fe011bd696097e3b6d
edKotinsky <edkotinsky@gmail.com>
2023-04-02 21:12:41 +0600

    multi_arg test

---
+++ test/multi_arg.cpp
@@ -0,0 +1,22 @@
+#include <argueme/arg.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+
+TEST_CASE("multi_argument") {
+  arg::command_line cmd("--", "-");
+
+  using svvec_t = std::vector<std::string_view>;
+  using svec_t = std::vector<std::string>;
+
+  SECTION("multi_argument allows to be appeared multiple times") {
+    svvec_t vec { "--message", "hello",     "-m",
+                                        "beautiful", "--message", "world" };
+
+    arg::multi_argument<std::string> msg("message", "m", cmd);
+
+    cmd.parse(vec);
+
+    svec_t expected {"hello", "beautiful", "world"};
+    REQUIRE(msg.get() == expected);
+  }
+}

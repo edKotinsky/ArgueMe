23217ab4a92fffcf91f42f86d9a4b9d7ba241491
edKotinsky <edkotinsky@gmail.com>
2023-04-08 18:57:35 +0600

    Change description; Add actions for hello_int and hello_float

--- examples/usage.cpp
+++ examples/usage.cpp
@@ -10,10 +10,10 @@ int main(int argc, char** argv) {
   arg::command_line cmd("--", "-");
 
   arg::value_argument<int> hello_int("int", "i", cmd);
-  hello_int.add_description("Hello int");
+  hello_int.add_description("Prints \"Hello int: <value>\"");
 
   arg::value_argument<float> hello_float("float", "f", cmd);
-  hello_float.add_description("Hello float");
+  hello_float.add_description("Prints \"Hello float: <value>\"");
 
   arg::switch_argument help("help", "h", cmd);
   help.add_description("Prints help message");
@@ -25,7 +25,16 @@ int main(int argc, char** argv) {
     print_help(cmd);
   }
 
-  if (help.get() || argc == 1) print_help(cmd);
+  if (help.get() || argc == 1) {
+    print_help(cmd);
+    return 0;
+  }
+
+  if (hello_int.get())
+    std::cout << "Hello int: " << hello_int.get() << std::endl;
+
+  if (hello_float.get())
+    std::cout << "Hello float: " << hello_float.get() << std::endl;
 
   return 0;
 }

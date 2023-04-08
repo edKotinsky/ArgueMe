ac59d9609f27316ca6bbf9f9b6826b67d259544d
edKotinsky <edkotinsky@gmail.com>
2023-04-08 19:32:40 +0600

    Add example description

--- examples/usage.cpp
+++ examples/usage.cpp
@@ -1,3 +1,87 @@
+/*
+ * ## Usage example of ArgueMe library.
+ *
+ * There are several cases with different behavior of a program:
+ *
+ * 1) Program name itself without any arguments: ./UsageExample
+ *    will cause help print:
+ *
+ * ```
+ * $ ./UsageExample
+ * /help message/
+ * ```
+ *
+ * 2) Invalid arguments (nonexistent arguments) will cause short error message
+ *    and help print:
+ *
+ * ```
+ * $ ./UsageExample -g
+ * Invalid argument: -g
+ * /help message/
+ * ```
+ *
+ * 3) -h and --help arguments will cause help message print.
+ *
+ * ```
+ * $ ./UsageExample -h
+ * /help message/
+ * ```
+ *
+ * 4) Arguments -i|--int, -f|--float without a following value (`./UsageExample
+ *    -i`) cause an error message and help message print.
+ *
+ * ```
+ * $ ./UsageExample -i
+ * Option requires a value: -i
+ * /help message/
+ * ```
+ *
+ * 5) Argument -i|--int with the proper value: -i|--int <value> and the same
+ *    with -f|--float argument cause the following output:
+ *
+ * ```
+ * $ ./UsageExample -i 10
+ * Hello int: 10
+ * $ ./UsageExample -f 3.14
+ * Hello float: 3.14
+ * ```
+ *
+ * 6) If a given value string cannot be converted to a type of an argument,
+ *    program will give the following output:
+ *
+ * ```
+ * $ ./UsageExample -i 3.14
+ * Cannot convert a string `3.14` to a value: -i
+ * /help message/
+ * ```
+ *
+ * 7) If the same argument appeared more than once:
+ *
+ * ```
+ * $ ./UsageExample -i 1 -i 2
+ * Option can be appeared only once: -i
+ * /help message/
+ * ```
+ *
+ * 8) User can pass both -i|--int and -f|--float arguments, but it does not work
+ * with -h|--help argument:
+ *
+ * ```
+ * $ ./UsageExample -i 1 -f 1.41
+ * Hello int: 1
+ * Hello float: 1.41
+ * $ ./UsageExample -f 1.41 -h
+ * /help message/
+ * ```
+ *
+ * In all code examples above the `/help message/` string stands for:
+ *
+ * ```
+ * -i, --int   Prints "Hello int: <value>"
+ * -f, --float Prints "Hello float: <value>"
+ * -h, --help  Prints help message
+ * ```
+ */
 #include <argueme/arg.hpp>
 #include <iostream>
 
